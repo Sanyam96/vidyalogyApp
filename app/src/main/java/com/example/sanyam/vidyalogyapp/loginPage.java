a31b5a2268da423a3ee2e5cadae5bb5d457664c0
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-03-22 02:19:10 +0530

    Updated Login Page Layout

--- app/src/main/java/com/example/sanyam/vidyalogyapp/loginPage.java
+++ app/src/main/java/com/example/sanyam/vidyalogyapp/loginPage.java
@@ -2,12 +2,30 @@ package com.example.sanyam.vidyalogyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class loginPage extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login_page);
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+
+/*            View decorView = getWindow().getDecorView();
+            // Hide both the navigation bar and the status bar.
+            // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+            // a general rule, you should design your app to hide the status bar whenever you
+            // hide the navigation bar.
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);*/
+
     }
 }
